fa58ad643d0449d323a8ae032bc73de5c5dac9eb
Ignacio Heredia <ignacio.nh@gmail.com>
2020-01-04 22:36:54 -0300

    Add tests for all the main class

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -6,9 +6,19 @@ const mockMediaUploader = (class {
 });
 jest.mock('../media-uploader', () => mockMediaUploader);
 
+const mockLoggerConstructor = jest.fn();
+const mockLoggerLog = jest.fn();
+const mockLogger = (class {
+  constructor(...args) { mockLoggerConstructor(...args) }
+  log(...args) { mockLoggerLog(...args) }
+});
+jest.mock('../logger', () => mockLogger);
+
 const mockTwitterConstructor = jest.fn();
+const mockTwitterPost = jest.fn();
 const mockTwitterClass = (class {
   constructor(...args) { mockTwitterConstructor(...args) }
+  post(url, params, callback) { callback(mockTwitterPost(url, params)) }
 });
 jest.mock('twitter', () => mockTwitterClass);
 const TweetBot = require('..');
@@ -18,13 +28,18 @@ describe('Tweet bot class', () => {
     mockMediaUploaderConstructor.mockClear();
     mockMediaUploaderUpload.mockClear();
     mockTwitterConstructor.mockClear();
+    mockTwitterPost.mockClear();
+    mockTwitterConstructor.mockClear;
+    mockLoggerLog.mockClear();
   });
 
-  it('should instantiate a Twitter client', () => {
-    const bot = new TweetBot({ secrets: 'some-secrets' });
+  it('should instantiate a Twitter client and logger', () => {
+    const bot = new TweetBot({ secrets: 'some-secrets', verbose: true });
     expect(bot.client).toBeInstanceOf(mockTwitterClass);
     expect(mockTwitterConstructor).toHaveBeenCalledTimes(1);
-    expect(mockTwitterConstructor).toHaveBeenCalledWith('some-secrets')
+    expect(mockTwitterConstructor).toHaveBeenCalledWith('some-secrets');
+    expect(mockLoggerConstructor).toHaveBeenCalledTimes(1);
+    expect(mockLoggerConstructor).toHaveBeenCalledWith({ verbose: true })
   });
 
   it('should resolve with no mediaId when mediaPath is undefined', async () => {
@@ -42,5 +57,49 @@ describe('Tweet bot class', () => {
     expect(mediaId).toBe('mocked-media-id');
     expect(mockMediaUploaderConstructor).toHaveBeenCalledTimes(1);
     expect(mockMediaUploaderUpload).toHaveBeenCalledTimes(1);
-  })
+  });
+
+  it('should publish a text-only tweet', async () => {
+    const bot = new TweetBot({ secrets: 'some-secrets' });
+    await bot.tweet({ status: 'Hello twitter' });
+    expect(mockTwitterPost).toHaveBeenCalledTimes(1);
+    expect(mockTwitterPost).toHaveBeenCalledWith('statuses/update', { status: 'Hello twitter', media_ids: null });
+    expect(mockLoggerLog).toHaveBeenCalledTimes(1);
+    expect(mockLoggerLog).toHaveBeenCalledWith('Tweeting "Hello twitter"');
+  });
+
+  it('should publish a media-only tweet', async () => {
+    mockMediaUploaderUpload.mockReturnValueOnce('media-id-123');
+    const bot = new TweetBot({ secrets: 'some-secrets' });
+    await bot.tweet({ mediaPath: '/path/to/some/file.gif' });
+    expect(mockTwitterPost).toHaveBeenCalledTimes(1);
+    expect(mockTwitterPost).toHaveBeenCalledWith('statuses/update', { status: '', media_ids: 'media-id-123' });
+    expect(mockLoggerLog).toHaveBeenCalledTimes(1);
+    expect(mockLoggerLog).toHaveBeenCalledWith('Tweeting with media /path/to/some/file.gif');
+  });
+
+  it('should publish a text-and-media tweet', async () => {
+    mockMediaUploaderUpload.mockReturnValueOnce('media-id-456');
+    const bot = new TweetBot({ secrets: 'some-secrets' });
+    await bot.tweet({ status: 'Check this thing', mediaPath: '/path/to/some/other/file.gif' });
+    expect(mockTwitterPost).toHaveBeenCalledTimes(1);
+    expect(mockTwitterPost).toHaveBeenCalledWith('statuses/update', { status: 'Check this thing', media_ids: 'media-id-456' });
+    expect(mockLoggerLog).toHaveBeenCalledTimes(1);
+    expect(mockLoggerLog).toHaveBeenCalledWith('Tweeting "Check this thing" with media /path/to/some/other/file.gif');
+  });
+
+  it('should log errors from the twitter client', async () => {
+    mockTwitterPost.mockReturnValueOnce('some-error');
+    const bot = new TweetBot({ secrets: 'some-secrets' });
+    let raisedError;
+    try {
+      await bot.tweet({ status: 'This should fail' });
+    } catch (error) {
+      raisedError = error;
+    }
+    expect(mockLoggerLog).toHaveBeenCalledTimes(2);
+    expect(mockLoggerLog).toHaveBeenCalledWith('Tweeting "This should fail"');
+    expect(mockLoggerLog).toHaveBeenCalledWith('some-error');
+    expect(raisedError).toBe('some-error');
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const Twitter = require('twitter');
 const MediaUploader = require('./media-uploader');
 const Logger = require('./logger');
@@ -10,6 +11,7 @@ class TweetBot {
   * */
   constructor ({secrets, verbose}) {
     this.client = new Twitter(secrets);
+    this.clientPost = util.promisify(this.client.post);
     this.verbose = verbose;
     this.logger = new Logger({ verbose });
   }
@@ -21,13 +23,15 @@ class TweetBot {
   async tweet ({status, mediaPath}) {
     const postOptions = {
       status: status || '',
-      media_ids: this._uploadMedia(mediaPath)
+      media_ids: await this._uploadMedia(mediaPath)
     };
-    this.logger.log(`Tweeting "${postOptions.status}${ postOptions.media_ids ? ` with media ${mediaPath}` : ''}"`);
-    this.client.post('statuses/update', postOptions, (error) => {
-      if (error) return this.logger.log(error);
-      this.logger.log(`Tweeted "${postOptions.status}" ✓`);
-    });
+    this.logger.log(`Tweeting${ postOptions.status ? ` "${postOptions.status}"` : ''}${ postOptions.media_ids ? ` with media ${mediaPath}` : ''}`);
+    try {
+      await this.clientPost('statuses/update', postOptions);
+    } catch (error) {
+      this.logger.log(error);
+      throw error;
+    }
   }
 
   async _uploadMedia(mediaPath) {
