6790c7e93781a7fcdf04d3521b9dde9c3328eaca
Ignacio Heredia <ignacio.nh@gmail.com>
2020-01-04 21:18:17 -0300

    Rewrite method using await/async and add test

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -1,18 +1,46 @@
+const mockMediaUploaderConstructor = jest.fn();
+const mockMediaUploaderUpload = jest.fn();
+const mockMediaUploader = (class {
+  constructor(...args) { mockMediaUploaderConstructor(...args) }
+  upload(...args) { return mockMediaUploaderUpload(...args) }
+});
+jest.mock('../media-uploader', () => mockMediaUploader);
+
 const mockTwitterConstructor = jest.fn();
 const mockTwitterClass = (class {
-  constructor(...args) {
-    mockTwitterConstructor(...args);
-  }
+  constructor(...args) { mockTwitterConstructor(...args) }
 });
 jest.mock('twitter', () => mockTwitterClass);
-jest.mock('../media-uploader', () => {});
 const TweetBot = require('..');
 
 describe('Tweet bot class', () => {
+  beforeEach(() => {
+    mockMediaUploaderConstructor.mockClear();
+    mockMediaUploaderUpload.mockClear();
+    mockTwitterConstructor.mockClear();
+  });
+
   it('should instantiate a Twitter client', () => {
     const bot = new TweetBot({ secrets: 'some-secrets' });
     expect(bot.client).toBeInstanceOf(mockTwitterClass);
     expect(mockTwitterConstructor).toHaveBeenCalledTimes(1);
     expect(mockTwitterConstructor).toHaveBeenCalledWith('some-secrets')
   });
+
+  it('should resolve with no mediaId when mediaPath is undefined', async () => {
+    const bot = new TweetBot({ secrets: 'some-secrets' });
+    const mediaId = await bot._uploadMedia();
+    expect(mediaId).toBeNull();
+    expect(mockMediaUploaderConstructor).toHaveBeenCalledTimes(0);
+    expect(mockMediaUploaderUpload).toHaveBeenCalledTimes(0);
+  });
+
+  it('should upload media using the MediaUploader', async () => {
+    mockMediaUploaderUpload.mockReturnValueOnce('mocked-media-id');
+    const bot = new TweetBot({ secrets: 'some-secrets' });
+    const mediaId = await bot._uploadMedia('/path/to/some/file');
+    expect(mediaId).toBe('mocked-media-id');
+    expect(mockMediaUploaderConstructor).toHaveBeenCalledTimes(1);
+    expect(mockMediaUploaderUpload).toHaveBeenCalledTimes(1);
+  })
 });
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ class TweetBot {
   * */
   constructor ({secrets, verbose}) {
     this.client = new Twitter(secrets);
-    this.preventTweet = false;
     this.verbose = verbose;
     this.log = (msg) => this.verbose ? console.log(msg) : null;
   }
@@ -18,37 +17,10 @@ class TweetBot {
   * status: string with the final tweet text
   * mediaPath: path to .gif or .mp4 (optional)
   * */
-  tweet ({status, mediaPath}) {
-    this.status = status || '';
-
-    if (this.preventTweet) {
-      const logMsg = `Prevented tweet`
-        + (this.status ? ` "${this.status}"` : '')
-        + (mediaPath ? ` with media ${mediaPath}` : '');
-      return this.log(logMsg);
-    }
-
-    let mediaPromise;
-    if (mediaPath) {
-      const mediaUploader = new MediaUploader({
-        client: this.client,
-        mediaPath: mediaPath,
-        verbose: this.verbose
-      });
-      mediaPromise = mediaUploader.upload()
-    } else {
-      mediaPromise = Promise.resolve()
-    }
-    mediaPromise.then(mediaId => {
-      this.mediaId = mediaId;
-      this._updateStatus();
-    })
-  }
-
-  _updateStatus () {
+  async tweet ({status, mediaPath}) {
     const postOptions = {
-      status: this.status,
-      media_ids: this.mediaId
+      status: status || '',
+      media_ids: this._uploadMedia(mediaPath)
     };
     this.log(postOptions);
     this.log(`Tweeting "${postOptions.status}"`);
@@ -57,6 +29,19 @@ class TweetBot {
       if (error) return this.log(error);
       this.log(`Tweeting "${postOptions.status}" ✓`);
     });
+  }
+
+  async _uploadMedia(mediaPath) {
+    let mediaId = null;
+    if (mediaPath) {
+      const mediaUploader = new MediaUploader({
+        client: this.client,
+        mediaPath: mediaPath,
+        verbose: this.verbose
+      });
+      mediaId = await mediaUploader.upload()
+    }
+    return mediaId
   }
 }
 
